465ae5a5e5bf1043f976522208b540829c13e5f1
Nelson Arabel <nelson@nelsons-MacBook-Pro.local>
2023-08-01 22:30:30 -0300

    first commit

---
+++ src/main/java/com/narabel/api_test_template/ApiTestTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.narabel.api_test_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTestTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/narabel/api_test_template/ApiTestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narabel.api_test_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
